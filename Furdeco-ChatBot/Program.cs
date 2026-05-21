706d11b5d3073f784a51c68f76f28e5593153f3e
pc <pc@DESKTOP-7II567Q>
2026-05-21 18:00:00 +0530

    changes

--- Furdeco-ChatBot/Program.cs
+++ Furdeco-ChatBot/Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddHttpClient<IVoodooSmsService, VoodooSmsService>();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
-builder.Services.AddHttpClient("GSIT", c => c.Timeout = TimeSpan.FromSeconds(20));
+builder.Services.AddHttpClient("GSIT", c => c.Timeout = TimeSpan.FromSeconds(45));
 
 builder.Services.AddCors(options =>
 {
